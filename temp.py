daec323b08ab9d7fd68e831d8a5fa9c2b693fd10
noachilles <kej010803@gmail.com>
2025-01-29 14:33:14 +0900

    2025-01-29

--- temp.py
+++ temp.py
@@ -17,3 +17,124 @@
 #         print(i, end='')
 #         array[i] -= 1
     
+# 백준 - Bronze - 2798
+
+# n, m = map(int, input().split())
+# array = list(map(int, input().split()))
+# res = sum = 0
+
+# for i in range(0, n):
+#     for j in range(i+1, n):
+#         for k in range(j+1, n):
+#             sum += array[i]
+#             sum += array[j]
+#             sum += array[k]
+#             if sum <= m:
+#                 res = max(res, sum)
+#             sum = 0
+# print(res)
+
+# 백준 - Silver - 11650
+
+# n = int(input())
+# array = []
+# for _ in range(n):
+#     x, y = map(int, input().split())
+#     array.append((x, y))
+
+# array = sorted(array, key=lambda x : (x[0], x[1]))
+# for t in array:
+#     print(t[0], t[1], end="\n")
+
+# 백준 - Silver - 11651
+
+# n = int(input())
+# array = []
+# for _ in range(n):
+#     x, y = map(int, input().split())
+#     array.append((x, y))
+
+# array = sorted(array, key=lambda x : (x[1], x[0]))
+# for t in array:
+#     print(t[0], t[1], end="\n")
+
+# 백준 - Silver - 10814
+
+# n = int(input())
+# d = dict()
+# for i in range(n):
+#     age, name = map(str, input().split())
+#     d[i] = (age, name)
+    
+# d = sorted(d.items(), key=lambda x: (x[1][0], x[0]))
+# for v in d:
+#     print(v[1][0], v[1][1], end='\n')
+
+# 백준 - Silver - 10815
+
+# array: 가지고 있는 숫자 카드
+# target: 가지고 있는지 아닌지
+
+# 방법 1. 
+# 교집합을 찾아서 -> 그 원소가 있는 위치를 찾아<< index 함수로, res[i] 값을 1로 만든다.
+# 원소의 위치를 찾아 res값을 변경
+
+# import sys
+
+# n = int(input())
+# array = sys.stdin.readline().rstrip()
+# array = array.split()
+# m = int(input())
+# target = sys.stdin.readline().rstrip()
+# target = target.split()
+# res = [0] * m
+
+# intersect = set(map(int, set(array) & set(target)))
+# for a in intersect:
+#     i = target.index(a)
+#     res[i] = 1
+# for _ in res:
+#     print(_, end=' ')
+
+# => runtime error
+    
+# 방법 2.
+# binary search 
+
+# import sys
+
+# def binary_search(array, target, start, end):
+#     while (start <= end):
+#         mid = (start + end) // 2
+#         if array[mid] == target:
+#             return 1
+#         elif array[mid] < target:
+#             start = mid + 1
+#         else:
+#             end = mid - 1            
+#     return 0
+
+# n = int(input())
+# array = sys.stdin.readline().rstrip()
+# array = list(map(int, array.split()))
+# array.sort()
+# m = int(input())
+# target = sys.stdin.readline().rstrip()
+# target = list(map(int, target.split()))
+# res = []
+
+# for t in target:
+#     res.append(binary_search(array, t, 0, n-1))
+
+# for _ in res:
+#     print(_, end=' ')
+
+# 정답 but 오래 걸림 -> dict() 사용하자
+
+# 백준 - Silver - 14425
+
+import sys
+n, m = map(int, sys.stdin.readline().rstrip())
+
+for _ in range(n):
+    sys.stdin.readline().rstrip()
