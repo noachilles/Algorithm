31cc580ecf9e4d6e03d0b8936dbf29a3be4885e1
noachilles <kej010803@gmail.com>
2025-02-23 10:34:39 +0900

    2025-02-22 Algorithm Solutions

---
+++ Solutions/dp.py
@@ -0,0 +1,29 @@
+# BOJ - Silver - 1003
+# 시간제한 0.25
+# Bottom Up으로 풀이하자
+# import sys
+# input = sys.stdin.readline
+
+# t = int(input())
+# while t:
+#     res = []
+#     n = int(input())
+#     res.append((1, 0))
+#     res.append((0, 1))
+#     if n < 2:
+#         print(res[n][0], res[n][1])
+#         t -= 1
+#         continue
+#     for i in range(2, n+1):
+#         num_0 = res[i-1][0] + res[i-2][0]
+#         num_1 = res[i-1][1] + res[i-2][1]
+#         res.append((num_0, num_1))
+#     print(res[n][0], res[n][1])
+#     t -= 1
+
+# BOJ - Silver - 9655
+# n = int(input())
+# if n % 2 == 0:
+#     print('CY')
+# else:
+#     print('SK')
---
+++ Solutions/implement.py
@@ -0,0 +1,20 @@
+# BOJ - Silver - 10773
+# a = []
+# res = 0
+# last = 0
+# k = int(input())
+# for _ in range(k):
+#     n = int(input())
+#     if n == 0:
+#         res -= last
+#         a.pop()
+#         if a:
+#             last = a[-1]
+#         continue
+#     a.append(n)
+#     res += n
+#     last = a[-1]
+# print(res)
+
+# BOJ - Silver - 1193
+# 0.5초, 추가시간 없음 - DP로 풀어야할 것 같은데
