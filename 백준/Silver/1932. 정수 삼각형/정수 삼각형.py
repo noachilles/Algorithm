5ea4961e9d8798d2d24f30c7d4ac43fce71b5b42
김은주 <74445032+noachilles@users.noreply.github.com>
2025-03-18 22:04:20 +0900

    [Silver I] Title: 정수 삼각형, Time: 120 ms, Memory: 42580 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
@@ -1,19 +1,13 @@
-import copy
-N = int(input())
-board = []
-res = -1e9
-for i in range(N):
-    data = list(map(int, input().split()))
-    board.append(data)
-for i in range(1, N): # 각 행에 대해
-    for j in range(i + 1): # 1행이 1개 값을 가지므로
-        if 0 < j < i:
-            board[i][j] = max(board[i-1][j-1], board[i-1][j]) + board[i][j]
-        elif j == 0:
-            board[i][j] = board[i-1][j] + board[i][j]
-        else:
-            board[i][j] = board[i-1][j-1] + board[i][j]
-for i in range(N):
-    if res < board[-1][i]:
-        res = board[-1][i]
-print(res)
+n = int(input())
+grid = [list(map(int, input().split())) for _ in range(n)]
+new = [[0] * n for _ in range(n)]
+new[0][0] = grid[0][0]
+
+def dp():
+    for i in range(n-1):
+        for j in range(i+1):
+            new[i+1][j] = max(new[i+1][j], new[i][j] + grid[i+1][j])
+            new[i+1][j+1] = max(new[i+1][j+1], new[i][j] + grid[i+1][j+1])
+
+dp()
+print(max(new[-1]))
