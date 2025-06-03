b8ad8ca6f4be9d8241c7753ef1533faecee39b60
김은주 <74445032+noachilles@users.noreply.github.com>
2025-06-03 16:36:18 +0900

    [Silver II] Title: 주식, Time: 3392 ms, Memory: 153708 KB -BaekjoonHub

--- 백준/Silver/11501. 주식/주식.py
+++ 백준/Silver/11501. 주식/주식.py
@@ -1,4 +1,5 @@
 T = int(input())
+
 for tc in range(1, T+1):
     N = int(input())
     lst = list(map(int, input().split()))
@@ -7,10 +8,8 @@ for tc in range(1, T+1):
     ans = 0
 
     for i in range(N-1, -1, -1):
-        if lst[i] > lst[mx]:
-            for j in range(i+1, mx, 1):
-                ans += (lst[mx] - lst[j])
+        if lst[i] < lst[mx]:
+            ans += (lst[mx] - lst[i])
+        else:
             mx = i
-    for i in range(mx):
-        ans += (lst[mx] - lst[i])
     print(ans)
