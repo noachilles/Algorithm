da7d38d8f40c9d5821384cbbc10328716e11e702
김은주 <74445032+noachilles@users.noreply.github.com>
2025-10-20 23:33:04 +0900

    [Bronze V] Title: A+B, Time: 172 ms, Memory: 17724 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) throws Exception{
+		// Scanner를 불러와서
+		Scanner scanner = new Scanner(System.in);
+		int A = scanner.nextInt();
+		int B = scanner.nextInt();
+		
+		System.out.println(A+B);
+	}
+}
